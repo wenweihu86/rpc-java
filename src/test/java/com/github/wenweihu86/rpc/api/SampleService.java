eb85f25c35bd40b959e6de44f2d52c0c1a5df055
wenweihu86 <wenweihu86@163.com>
2018-02-26 16:18:41 +0800

    添加RPCMeta使用示例

--- src/test/java/com/github/wenweihu86/rpc/api/SampleService.java
+++ src/test/java/com/github/wenweihu86/rpc/api/SampleService.java
@@ -1,8 +1,15 @@
 package com.github.wenweihu86.rpc.api;
 
+import com.github.wenweihu86.rpc.protocol.RPCMeta;
+
 /**
  * Created by wenweihu86 on 2017/4/25.
  */
 public interface SampleService {
+    /**
+     * 当需要定制serviceName和methodName时，用RPCMeta注解。
+     * RPCMeta可选，默认是通过反射获取。
+     */
+    @RPCMeta(serviceName = "SampleService", methodName = "sampleRPC")
     Sample.SampleResponse sampleRPC(Sample.SampleRequest request);
 }
